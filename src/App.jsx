208a888fe9519456bcd4227e12d3e7486c1f989a
FrankmDev <frankmmarindev@gmail.com>
2023-12-02 11:35:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Drum Machine</h1>
+      <main id="drum-machine">
+        <section className="pad"></section>
+      </main>
+    </>
+  );
+}
+
+export default App;
