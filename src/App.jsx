50c036a794bd26b1eff2d38bbacb1ccfbc9b6ab8
FrankmDev <frankmmarindev@gmail.com>
2023-12-03 21:01:31 +0100

    Drumpad

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import "./App.css";
+
+const DrumPad = ({ ident, btnKey, source }) => {
+  return (
+    <div className="drum-pad" id={ident}>
+      <audio id={btnKey} src={source}></audio>
+      {btnKey}
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
       <h1>Drum Machine</h1>
       <main id="drum-machine">
-        <section className="pad"></section>
+        <section className="pad">
+          <DrumPad
+            ident={"Heater-1"}
+            bntKey={"Q"}
+            source={"https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"}
+          />
+        </section>
+        <section id="display"></section>
       </main>
     </>
   );
